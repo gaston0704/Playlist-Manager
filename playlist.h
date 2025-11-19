c963d0a900b5792072d68c40357c94382c69ff69
gaston0704 <163434650+gaston0704@users.noreply.github.com>
2025-11-19 23:55:32 +0200

    Enhance Playlist class with move semantics
    Added move constructor and assignment operator for Playlist class.

--- playlist.h
+++ playlist.h
@@ -3,11 +3,15 @@
 
 #include "song.h"
 #include <cstddef>
+#include <utility>
+#include <iostream>
 
 class Playlist{
 private:
     Song** songs;
     size_t count;
+
+    void init_form(const Playlist& other); 
 
 public:
     Playlist(); //constructor
@@ -17,8 +21,15 @@ public:
 
     Playlist(Playlist&& anotherPlaylist) noexcept; //move constructor
 
+    Playlist& operator = (Playlist rhs);
+    void swap(Playlist& other) noexcept;
+
     void addSong(const Song& song);
     void print() const;
 };
+
+inline void swap(Playlist& a, Playlist& b) noexcept {
+    a.swap(b);
+}
 
 #endif
